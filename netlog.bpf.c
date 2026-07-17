675d871185183d9e40d566a4d1815344140c6695
map38000-rgb <map38000@gmail.com>
2026-07-17 17:11:24 +0700

    Update netlog.bpf.c

--- netlog.bpf.c
+++ netlog.bpf.c
@@ -1,16 +1,30 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
-#include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-/* Hook vào hàm tcp_connect của kernel - Dùng định nghĩa hàm C chuẩn */
+/* 
+ * Tự định nghĩa cách lấy tham số đầu tiên (RDI trên x86_64, regs[0] trên ARM64)
+ * Tránh sử dụng bpf_tracing.h của hệ thống để không bị xung đột struct pt_regs
+ */
+static __always_inline unsigned long get_first_param(struct pt_regs *ctx) {
+#if defined(__TARGET_ARCH_x86)
+    // Trên x86_64, tham số 1 nằm ở thanh ghi di (hoặc rdi)
+    return ctx->di;
+#elif defined(__TARGET_ARCH_arm64)
+    // Trên arm64, tham số 1 nằm ở regs[0]
+    return ctx->regs[0];
+#else
+    return 0;
+#endif
+}
+
 SEC("kprobe/tcp_connect")
 int BPF_KPROBE_PROTOTYPE_FIX(struct pt_regs *ctx)
 {
-    /* Lấy tham số đầu tiên của hàm tcp_connect (chính là struct sock *sk) dựa trên cấu trúc của ctx */
-    struct sock *sk = (struct sock *)PT_REGS_PARM1_CORE(ctx);
+    // Lấy con trỏ struct sock *sk từ tham số đầu tiên của hàm tcp_connect
+    struct sock *sk = (struct sock *)get_first_param(ctx);
     if (!sk)
         return 0;
 
@@ -18,13 +32,13 @@ int BPF_KPROBE_PROTOTYPE_FIX(struct pt_regs *ctx)
     char comm[16];
     u32 saddr = 0, daddr = 0;
 
-    /* Lấy PID (32-bit cao của bpf_get_current_pid_tgid) */
+    /* Lấy PID (32-bit cao) */
     pid = bpf_get_current_pid_tgid() >> 32;
 
-    /* Lấy tên tiến trình (package/command name) */
+    /* Lấy tên tiến trình */
     bpf_get_current_comm(&comm, sizeof(comm));
 
-    /* Đọc an toàn địa chỉ IP nguồn và đích bằng CO-RE */
+    /* Đọc an toàn địa chỉ IP bằng CO-RE */
     BPF_CORE_READ_INTO(&saddr, sk, __sk_common.skc_rcv_saddr);
     BPF_CORE_READ_INTO(&daddr, sk, __sk_common.skc_daddr);
 
